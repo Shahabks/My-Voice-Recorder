5fed67d870c2933ea1d01e215fd3de93e608146b
Shahab Sabahi <27753966+Shahabks@users.noreply.github.com>
2019-02-05 20:48:57 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,73 @@
+from setuptools import setup
+
+long_description="""My-Voice-Recorder is a convenient and simple python package that can
+be used right in your machine. It allows you to record your voice using a microphone 
+and save it as an wav file. 
+
+My-Voice-Recorder was originally designed as a voice recorder for acoustic and language 
+research or creative endeavors. It was tested for our Automatic language identification 
+package and has a flexible adjustable and simple features for manual control which make 
+it more adaptable for specific needs.  
+These features not only make the device easier to use in an original environment, they 
+also help for optimum file storage opportunities.
+
+My-Voice-Recorder records in a wide range of quality such as CD-quality 16 or 24-bit 
+linear PCM or other formats and automatically detect your machine's microphones. 
+These are adjustable for varied stereo depth depending on your recording needs. It 
+could support multi-track recording, all of which are great features for creative 
+projects, as they allow for more control over the audio layers. 
+The main downside to My-Voice-Recorder is that it does not include voice activation or 
+dead air removal.
+
+Adding an external microphone is the single most significant way to improve the quality
+of the sound your machine captures. To give the voice recordings top-level audio, you'll 
+need to get a standard mic.
+
+MY-Voice-Recorder works with USB better though it is not a must. While the microphone quality 
+usually serves as a good indicator of recorded voice quality, a few mic factors have an 
+impact. Checking the mic frequency response (the range of sounds it can hear) and sampling rate.
+
+=============
+Installation
+=============
+My-Voice-Recorder can be installed like any other Python library, using (a recent version of) the
+Python package manager pip, on Linux, macOS, and Windows:
+
+------------------pip install myVOCRec
+				
+or, to update your installed version to the latest release:
+------------------- pip install -u myVOCRec	---------------------------------
+
+Recording files must be 25 sec. or longer of audio and in *.wav PCM/LPCM format, recorded at 48 kHz 
+sample frame and 24-32 bits of resolution or AIFF, AIFF-C, FLAC: must be native FLAC format; 
+OGG-FLAC is not supported.
+
+please check out https://github.com/Shahabks/my-voice-recorder   
+
+myVOCRec was developed by MYOLUTION Lab in Japan. It is being used internally where the quality of 
+utterances is crucial for our Voice Recognition and Acoustic & Language modeling Project in the Lab. 
+We plan to enrich the quality of myVOCRec by adding more advanced functions."""
+	
+	
+setup(name='myVOCRec',
+      version='1',
+      description='Capturing Microphone Data into an Audio File',
+	  long_description=long_description,
+	  url='https://github.com/Shahabks/myspokenlanguageid',
+      author='Shahab Sabahi',
+      author_email='sabahi.s@mysol-gc.jp',
+      license='MIT',
+      classifiers=[
+		'Intended Audience :: Developers',
+		'Intended Audience :: Science/Research',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3.7',
+		],
+	  keywords='speech signal processing , Natural Language Processing and Understanding , Voice-Recorder',
+	  install_requires=[
+		'sounddevice>=0.3.12',
+		'SoundFile>=0.10.2',
+		],
+	  packages=['myVOCRec'],
+      zip_safe=False)
+	  
